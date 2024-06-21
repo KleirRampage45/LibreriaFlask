b233de4454a2b8d745f6c45173ffc5b1d302cfe7
JoseEstCan <127878632+JoseEstCan@users.noreply.github.com>
2024-06-20 22:08:46 -0400

    Versión Final

--- LibreriaFlask/main.py
+++ LibreriaFlask/main.py
@@ -1,15 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
-
 from datetime import datetime, timedelta
-=======
-from werkzeug.security import generate_password_hash, check_password_hash
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///biblioteca.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 app.secret_key = 'supersecretkey'  # Cambia esto por una clave secreta más segura
 db = SQLAlchemy(app)
 
@@ -30,12 +25,6 @@ class Author(db.Model):
     genre_id = db.Column(db.Integer, db.ForeignKey('genre.id'), nullable=False)
     book_count = db.Column(db.Integer, default=0)
     books = db.relationship('Book', backref='author', lazy=True)
-
-=======
-app.config['SECRET_KEY'] = 'tu_clave_secreta_aqui'  # Clave secreta para proteger las sesiones y cookies
-db = SQLAlchemy(app)
-
-# Define el modelo de Libro
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -79,7 +68,6 @@ class BookReview(db.Model):
 
 # Crear todas las tablas definidas en la base de datos
 db.create_all()
-
 
 # Agregar el usuario inicial
 if not User.query.filter_by(username='admin').first():
@@ -185,40 +173,9 @@ def edit_book(id):
     authors = Author.query.all()
     genres = Genre.query.all()
     return render_template("edit.html", book=book, authors=authors, genres=genres)
-=======
-# Ruta para la página principal
-@app.route('/')
-def home():
-    all_books = Book.query.all()
-    return render_template('index.html', books=all_books)
-
-# Ruta para la página de agregar libro
-@app.route('/add', methods=['GET', 'POST'])
-def add_book():
-    if request.method == 'POST':
-        title = request.form['title']
-        author = request.form['author']
-        rating = request.form['rating']
-        new_book = Book(title=title, author=author, rating=rating)
-        db.session.add(new_book)
-        db.session.commit()
-        return redirect(url_for('home'))
-    return render_template('add.html')
-
-# Ruta para la página de editar calificación
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
-def edit_rating(id):
-    book = Book.query.get_or_404(id)
-    if request.method == 'POST':
-        book.rating = request.form['new_rating']
-        db.session.commit()
-        return redirect(url_for('home'))
-    return render_template('edit.html', book=book)
-
 
 @app.route('/delete/<int:id>')
 def delete_book(id):
-
     if 'user_id' not in session or User.query.get(session['user_id']).profile != 'Administrador':
         return redirect(url_for('login'))
     book = Book.query.get(id)
@@ -338,39 +295,4 @@ def view_reviews(book_id):
 
 if __name__ == "__main__":
     app.run(debug=True)
-=======
-    book = Book.query.get_or_404(id)
-    db.session.delete(book)
-    db.session.commit()
-    return redirect(url_for('home'))
 
-# Ruta para la página de registro de usuario
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-
-        # Verificar si el usuario ya existe en la base de datos
-        existing_user = User.query.filter_by(username=username).first()
-        if existing_user:
-            return "Ya existe un usuario con ese nombre de usuario. Por favor, elige otro."
-
-        # Crear un nuevo usuario
-        new_user = User(username=username, email=email)
-        new_user.set_password(password)
-
-        # Añadir el nuevo usuario a la base de datos
-        db.session.add(new_user)
-        db.session.commit()
-
-
-        # Redirigir a la página de inicio de sesión u otra página según tu flujo de la aplicación
-        return redirect(url_for('home'))
-
-    # Renderizar el formulario de registro
-    return render_template('register.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
